f2aa5e1ad24775350981ba6d99fb3ea9f8653c85
CarlRon <cramos25@gmail.com>
2022-01-24 23:31:49 -0300

    Solo falta contacto

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let menu = document.querySelector('.menu')
-let openMenuBtn = document.querySelector('.open-menu')
-let closeMenuBtn = document.querySelector('.close-menu')
+const menu = document.querySelector('.menu')
+const openMenuBtn = document.querySelector('.open-menu')
+const closeMenuBtn = document.querySelector('.close-menu')
 
 function toggleMenu() {
 	menu.classList.toggle('menu_opened')
@@ -8,3 +8,32 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener('click', toggleMenu)
 closeMenuBtn.addEventListener('click', toggleMenu)
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]')
+
+let observer = new InteractionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			const id = entry.target.getAttribute('id')
+			const menuLink = document.querySelector(`.menu a[href="#${id}"]`)
+
+			if (entry.isIntersecting) {
+				document.querySelector('.menu a.selected').classList.remove('selected')
+				menuLink.classList.add('selected')
+			}
+		})
+	},
+	{ rootMargin: '-30% 0px -70% 0px' }
+)
+
+menuLinks.forEach((menuLink) => {
+	menuLink.addEventListener('click', function () {
+		menu.classList.remove('menu_opened')
+	})
+
+	const hash = menuLink.getAttribute('href')
+	const target = document.querySelector(hash)
+	if (target) {
+		observer.observe(target)
+	}
+})
